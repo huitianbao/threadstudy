23be6a5591182e89c676ba54e05e010bffbb653b
htb <5446944+htbcode@user.noreply.gitee.com>
2021-07-13 21:42:37 +0800

    Initial commit

---
+++ src/test/java/com/htb/threadstudy/ThreadstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htb.threadstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
